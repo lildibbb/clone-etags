4cc1a5a022b130c0246a05e023469dd12498b4d1
lildibbb <adib.asyraaf760@gmail.com>
2025-01-16 03:18:47 +0800

    feat: add package

--- src/routes/dashboard/pengadu/_pengadu.tsx
+++ src/routes/dashboard/pengadu/_pengadu.tsx
@@ -3,19 +3,18 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 import { Outlet, createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/dashboard/pengadu/_pengadu")({
-	component: RouteComponent,
+  component: RouteComponent,
 });
 
 function RouteComponent() {
-	return (
-		<div className="flex min-h-screen">
-			<SidebarProvider>
-				<AppSidebar userType="pengadu" />
-			</SidebarProvider>
-
-			<div className="flex-1 flex flex-col overflow-hidden">
-				<Outlet />
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex min-h-screen">
+      <SidebarProvider>
+        <AppSidebar userType="pengadu" />
+        <div className="flex-1 flex flex-col">
+          <Outlet />
+        </div>
+      </SidebarProvider>
+    </div>
+  );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,24 @@ import { defineConfig } from "vite";
 
 import viteReact from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import svgr from "vite-plugin-svgr";
 import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), viteReact()],
+  plugins: [
+    TanStackRouterVite(),
+    viteReact(),
+    svgr({
+      // svgr options: https://react-svgr.com/docs/options/
+      svgrOptions: {
+        exportType: "default",
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: "**/*.svg",
+    }),
+  ],
   server: {
     port: 3000,
     open: true,
